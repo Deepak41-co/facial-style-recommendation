31e3a0b0c1fb4fb2ee2e8df6d9f356c24a815287
Deepak41-co <your-github-email@example.com>
2025-10-27 18:08:16 +0530

    Added missing os import and minor fixes

--- augmented.py
+++ augmented.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import base64
 import hashlib
-
+import os
 app = Flask(__name__)
 CORS(app)
 from flask import send_from_directory
